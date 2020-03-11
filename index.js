14811d1e9c9c7acdd27af8c94e93280a00877693
Robert Kulp <pedalinon@gmail.com>
2020-03-11 13:47:52 -0700

    get users, user:id and post new user completed

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
 // implement your API here
+const express = require('express'); // A path goes here when needed. 
+// This access modules installed. 
+
+const db = require('./data/db.js');// .js is not requeired. 
+// calls local database file. 
+
+const server = express(); // returns a server object. 
+
+// TCP/IP goes in parameters of the function below. 
+// 4000 is the designated port
+server.listen(4000, () => {
+	console.log('listening on port 4000...');
+})
+// Places in listen mode. 
+
+
+// Don't include a path for use to call all paths. 
+// All requests will go through the use method. 
+server.use(express.json());
+
+server.get('/', (req, res) => {
+	res.send(req.params); // Printed in the browser
+})
+
+server.get('/api/users', (req, res) => {
+	// res.send(req.body);
+
+	db.find()
+		.then(user => {
+			res.status(200).json({user});
+		})
+		.catch(err => {
+			res.status(500).json({sucess: false, message: 'The users information could not be retreieved.'});
+		});
+})
+
+server.post('/api/users', (req, res) => {
+	const hubInfo = req.body;
+
+	db.insert(hubInfo)
+		.then(user => {
+			res.status(200).json({user});
+		})
+		.catch(err => {
+			res.status(400).json({success: false, message: 'Please provide name and bio for the user.'})
+		})
+})
+
+server.get('/api/users/:id', (req, res) => {
+	const { id } = req.params;
+
+	db.findById(id)
+		.then(userId => {
+			res.status(200).json({userId});
+		})
+		.catch(err => {
+			res.status(404).json({success: false, message: 'Not Found'})
+		})
+})
