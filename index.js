48f5219f01101023b7e4271873c06b3e25722bfa
Robert Kulp <pedalinon@gmail.com>
2020-03-11 20:34:12 -0700

    some formattine

--- index.js
+++ index.js
@@ -88,23 +88,23 @@ server.delete('/api/users/:id', (req, res) => {
 server.put('/api/users/:id', (req, res) => {
 	const { name, bio } = req.body;
 
-		if (!name || !bio) {
-			res.status(400)
-		  		.json({ errorMessage: 'Please provide name and bio for the user.' });
-			} else {
-		db.update(req.params.id, req.body)
-			.then(user => {
-		if (user) {
-		  res.status(200).json(user);
-			} else {
-		  	res.status(404)
-		    	.json({ message: 'The user with the specified ID does not exist.'});
-			}
-		})
-		.catch(() => {
-		res.status(500).json({
-		  errorMessage: 'The user information could not be modified.',
-			});
+	if (!name || !bio) {
+		res.status(400)
+	  		.json({ errorMessage: 'Please provide name and bio for the user.' });
+		} else {
+	db.update(req.params.id, req.body)
+		.then(user => {
+	if (user) {
+	  res.status(200).json(user);
+		} else {
+	  	res.status(404)
+	    	.json({ message: 'The user with the specified ID does not exist.'});
+		}
+	})
+	.catch(() => {
+	res.status(500).json({
+	  errorMessage: 'The user information could not be modified.',
 		});
+	});
   }	
 })
